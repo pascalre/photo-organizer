46ff33490b057da417dc153b4feace56af3518d5
Pascal Reitermann <pascal@Pascals-Air.fritz.box>
2017-12-14 01:11:04 +0100

    Renamed script and added some log-output.

--- photo-organizer.sh
+++ photo-organizer.sh
@@ -6,20 +6,22 @@ for FILENAME in $SEARCHBASE
 do
   if test -d "$FILENAME"
   then
+    echo "Skipping directory '$FILENAME'."
     continue
   fi
   FILEEXT="${FILENAME##*.}"
  
   # Check if FILENAME is an image and has the Key "Exif.Photo.DateTimeOriginal"
   exiv2 -K Exif.Photo.DateTimeOriginal "$FILENAME" > /dev/null 2>&1
-  if [[ $? != 0 ]]
+  if [[ $? -ne 0 ]]
   then
+    echo "Skipping file '$FILENAME', because the key 'Exif.Photo.DateTimeOriginal' is missing."
     continue
   fi
 
   RNDM=$((100 + RANDOM % 899))
 
   #exiv2 -r %Y%m%d_%H%M%S_$RNDM $FILENAME
-  echo "Renamed $FILENAME"
+  echo "Renamed '$FILENAME'."
 
 done 
