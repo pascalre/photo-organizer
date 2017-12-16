07b8d1b848b9a5e65dc248a5c177272f040718af
Pascal Reitermann <pascal@Pascals-Air.fritz.box>
2017-12-16 14:09:10 +0100

    Used exiftool instead of exiv2, since it can also process video-files.

--- photo-organizer.sh
+++ photo-organizer.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-SEARCHBASE=*
-
-for FILENAME in $SEARCHBASE
-do
-  if test -d "$FILENAME"
-  then
-    echo "Skipping directory '$FILENAME'."
-    continue
-  fi
-  FILEEXT="${FILENAME##*.}"
- 
-  # Check if FILENAME is an image and has the Key "Exif.Photo.DateTimeOriginal"
-  exiv2 -K Exif.Photo.DateTimeOriginal "$FILENAME" > /dev/null 2>&1
-  if [[ $? -ne 0 ]]
-  then
-    echo "Skipping file '$FILENAME', because the key 'Exif.Photo.DateTimeOriginal' is missing."
-    continue
-  fi
-
-  RNDM=$((100 + RANDOM % 899))
-
-  #exiv2 -r %Y%m%d_%H%M%S_$RNDM $FILENAME
-  echo "Renamed '$FILENAME'."
-
-done 
+exiftool -v -r -d ./%Y/%m/%y-%m-%d/%Y%m%d_%H%M%S%%+2c.%%e '-FileName<CreateDate' *
