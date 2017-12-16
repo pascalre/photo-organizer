5379029e55efcd6f107de87f573add432b497c6c
Pascal Reitermann <pascal@Pascals-Air.fritz.box>
2017-12-16 14:54:01 +0100

    Adding a description.

--- photo-organizer.sh
+++ photo-organizer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Description 
+# ----------------------------------------------------------------------------------
+# This command helps to organise pictures and videos, that contain Exif-Metadata. It 
+# sorts all the files in a specific directory-structure and renames them following 
+# this pattern: "./YYYY/MM/YYYY-MM-DD/YYYYMMDD_HHMMSS.EXT".
+
 exiftool -v -r -d ./%Y/%m/%y-%m-%d/%Y%m%d_%H%M%S%%+2c.%%e '-FileName<CreateDate' *
